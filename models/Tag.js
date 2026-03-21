cdc3135283b40d63ae59d0e6fb837b751c1bb434
vornwebsite-web <vornwebsite@gmail.com>
2026-03-21 08:18:22 +0200

    Update Tag.js

--- models/Tag.js
+++ models/Tag.js
@@ -1,1 +1,16 @@
+'use strict';
 
+const { Schema, model } = require('mongoose');
+
+const TagSchema = new Schema({
+  guildId:      { type: String, required: true },
+  name:         { type: String, required: true },
+  content:      { type: String, required: true },
+  authorId:     { type: String, required: true },
+  uses:         { type: Number, default: 0     },
+  lastEditedAt: { type: Date,   default: null  },
+}, { timestamps: true });
+
+TagSchema.index({ guildId: 1, name: 1 }, { unique: true });
+
+module.exports = model('Tag', TagSchema);
