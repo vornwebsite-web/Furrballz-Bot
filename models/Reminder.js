7e9cbc43d6a1cdddf1e701522706d452a8c83513
vornwebsite-web <vornwebsite@gmail.com>
2026-03-21 08:15:32 +0200

    Update Reminder.js

--- models/Reminder.js
+++ models/Reminder.js
@@ -1,1 +1,17 @@
+'use strict';
 
+const { Schema, model } = require('mongoose');
+
+const ReminderSchema = new Schema({
+  userId:    { type: String, required: true },
+  guildId:   { type: String, default: null  },
+  channelId: { type: String, default: null  },
+  message:   { type: String, required: true },
+  fireAt:    { type: Date,   required: true },
+  fired:     { type: Boolean,default: false },
+}, { timestamps: true });
+
+ReminderSchema.index({ fireAt: 1, fired: 1 });
+ReminderSchema.index({ userId: 1, fired: 1 });
+
+module.exports = model('Reminder', ReminderSchema);
